4b7aaa1409d04de5a2cdc95b78337bbc96d3d8f1
Jim Kadin <james.kadin@jax.org>
2016-05-11 17:18:15 -0400

    Cleaned up webapp, using the new SciDirect.py. Needs more work, but at a stable point. 	deleted:    refUtils/jakUtils.py 	modified:   webapp/Refs.py 	modified:   webapp/static/style.css 	modified:   webapp/templates/layout.html 	new file:   webapp/templates/main_page.html 	modified:   webapp/templates/query_form.html 	modified:   webapp/templates/query_results.html

--- webapp/Refs.py
+++ webapp/Refs.py
@@ -1,14 +1,20 @@
 
+import sys
+import traceback
 import json
-from SciDirect import ElsevierSciDirect
+import string
+import urllib
+import urllib2
 from flask import Flask, url_for, render_template, request
+from SciDirect import SciDirectConnection
 
 app = Flask(__name__)
-elsevierConnect = ElsevierSciDirect()
+elsevierConnect = SciDirectConnection()
+querystring = ''	# most recent query string
 
 @app.route("/", methods=['POST', 'GET'])
 def base():
-    return render_template('query_form.html')
+    return render_template('main_page.html', querystring=querystring)
 
 @app.route("/login/")
 @app.route("/logout/")
@@ -17,26 +23,54 @@ def login():
 
 @app.route('/process_form/')
 def process_form():
-    query = request.args.get('query', '')
-    if True:
-	datadump = "method=%s... ...query='%s'" % (request.method, query)
+
+    global querystring
+    querystring = request.args.get('query', '')
+    journals = request.args.getlist('journals')
+    wholeQuery = constructQueryString(querystring, journals)
+    #return wholeQuery
+    if False:
+	datadump = "method=%s... ...query='%s'" % (request.method, wholeQuery)
 	app.logger.debug(datadump)
 
-    totalCount = elsevierConnect.doCount(query=query)
-    results = elsevierConnect.doQuery(query=query,maxrslts=5)
-    if type(results) == type("x"):	# connect error
-	app.logger.debug(results)
-	return results
-
-    jsonDump = json.dumps( elsevierConnect.doQuery_json(query=query),
-		    sort_keys=True, indent=2, separators=(',', ': ') )
+    message    = None
+    totalCount = 0
+    results    = []
+    jsonDump   = None
+    try:
+	totalCount = elsevierConnect.doCount(query=wholeQuery)
+	results = elsevierConnect.doQuery(query=wholeQuery,numToGet=5)
+	jsonDump = json.dumps( elsevierConnect.doQuery_Json(query=wholeQuery),
+		    sort_keys=False, indent=2, separators=(',', ': ') )
+    except urllib2.HTTPError as e:
+	if hasattr( e, 'code') and e.code == 400:
+					    # probably query syntax error
+	    message =  "HTTP error: You probably have an error in your query syntax."
+	    # write stack trace and exception
+	    e_type, e_value, e_tb = sys.exc_info()
+	    app.logger.debug( string.join(
+			traceback.format_exception(e_type, e_value, e_tb)) )
+	else:
+	    raise
 
     return render_template('query_results.html', 
-			    querystring=query,
+			    querystring=wholeQuery,
+			    message=message,
 			    totalCount=totalCount,
 			    count=len(results),
 			    results=results,
 			    jsonDump=jsonDump)
+
+def constructQueryString( base, journals):
+    query = base
+    if len(journals) > 0:
+	if query != '': query += " AND "
+	query = query + 'SRCTITLE("%s"' % journals[0]
+	for j in journals[1:]:
+	    query = query + ' OR "%s"' % j
+	query = query + ")"
+    return query
+
 
 if __name__ == "__main__":
     app.debug = True
